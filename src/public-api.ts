9dd1b12c635d6b19f5f8d561175e2d896859a9b6
Hyeonjun.oh <dhguswns23@naver.com>
2019-05-01 16:26:51 +0900

    Feat for add timeline to export entry.

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,8 +1,5 @@
-/*
- * Public API Surface of fizz
- */
-
 export * from './lib/fizz.service';
 export * from './lib/fizz.component';
 export * from './lib/fizz.module';
 export * from './fiz-icon-set.module';
+export * from './timeline';
